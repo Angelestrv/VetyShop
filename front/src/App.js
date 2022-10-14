41a079a10613754146c1ab299d331662e6909d50
Angelestrv <111291282+Angelestrv@users.noreply.github.com>
2022-10-14 18:45:32 +0200

    Clase del 14 de Octubre de 2022

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import React from 'react';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Tienda de mascotas Vety Shop</h1>
-      <img src="./images/vetyshop.png" alt="Aqui va el logo"></img>
+      <Header />
+      <center>Contenido aquí</center>
+      <center>Contenido aquí</center>
+      <center>Contenido aquí</center>
+      <center>Contenido aquí</center>
+      <center>Contenido aquí</center>
+      <center>Contenido aquí</center>
+      <center>Contenido aquí</center>
+      <center>Contenido aquí</center>
+      <center>Contenido aquí</center>
+      <Footer />
     </div>
   );
 }
